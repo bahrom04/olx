a85d49b8385f807cca218537419cba12ac71ee43
bahromoken <magdiyevbahrom@gmail.com>
2023-11-05 15:09:07 +0500

    gitignore

--- results/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Students, Regions, Districts, Schools
-
-# Register your models here.
-
-admin.site.register(Students)
-admin.site.register(Regions)
-admin.site.register(Districts)
-admin.site.register(Schools)
